2b6a6c070a3c97443fa51097fa131041fecce929
Mell <mellkayrusson@gmail.com>
2020-03-10 11:36:46 +0000

    login page and route working

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,70 @@
-import React from "react";
-import css from "./App.module.css";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "../Login/Login";
+import RegisterUser from "../RegisterProvider/registerProvider";
+import logo from "../../components/logo.svg";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+  function handleUsername(event) {
+    setUsername(event.target.value);
+  }
+  function handlePassword(event) {
+    setPassword(event.target.value);
+  }
+  function createUser() {
+    const userData = { username: username, password: password };
+    fetch(`http://localhost:5000/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userData)
+    })
+      .then(response => response.json())
+      .then(data => setLoggedIn(data.success))
+      .catch(err => console.log(err));
+  }
+  function submitLoginInfo() {
+    const userData = { username: username, password: password };
+    fetch(`http://localhost:5000/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userData)
+    })
+      .then(response => response.json())
+      .then(data => setLoggedIn(data.success))
+      .catch(err => console.log(err));
+  }
+
   return (
-    <div className={css.container}>
-      <h1>Happy Hacking!</h1>
+    <div className="App">
+      <header>
+        <img src={logo} style={{ width: "200px", margin: "10px" }} />
+      </header>
+      <hr />
+
+      <Router>
+        <Switch>
+          <div>
+            <Route path="/login">
+              <Login
+                handleUsername={handleUsername}
+                handlePassword={handlePassword}
+                submitLoginInfo={submitLoginInfo}
+                createUser={createUser}
+              />
+            </Route>
+            <Route path="/register">
+              <RegisterUser />
+            </Route>
+          </div>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Login.css";
+
+function Login({ handleUsername, handlePassword, submitLoginInfo }) {
+  return (
+    <div className="login">
+      <input placeholder="username" onChange={handleUsername} /> <br />
+      <input
+        type="password"
+        placeholder="password"
+        onChange={handlePassword}
+      />{" "}
+      <br />
+      <button onClick={submitLoginInfo}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
