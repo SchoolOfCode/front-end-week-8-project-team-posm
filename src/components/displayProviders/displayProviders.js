38f4dbf99ba6450d0b304c2e9354fa6d5471fb85
PatrickYoung03 <patrickyoung18@googlemail.com>
2020-03-12 14:46:00 +0000

    checkbox

--- src/components/displayProviders/displayProviders.js
+++ src/components/displayProviders/displayProviders.js
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-function DisplayProviders() {
-  const [providers, setProviders] = useState([]);
+// function DisplayProviders() {
+//   const [providers, setProviders] = useState([]);
 
-  useEffect(() => {
-    fetch(`http://localhost:5000/providers`)
-      .then(res => res.json())
-      .then(data => {
-        console.log({ data });
-        setProviders(data);
-      });
-  }, []);
+//   useEffect(() => {
+//     fetch(`http://localhost:5000/providers`)
+//       .then(res => res.json())
+//       .then(data => {
+//         console.log({ data });
+//         setProviders(data);
+//       });
+//   }, []);
 
-  //     async function getUserData() {
-  //       const res = await fetch(`http://localhost:5000/providers`);
-  //       console.log(res);
-  //       const data = await res.json();
-  //       console.log(data);
-  //     }
-  //     getUserData();
-  //     setProviders(data);
-  //   }, []);
+//   //     async function getUserData() {
+//   //       const res = await fetch(`http://localhost:5000/providers`);
+//   //       console.log(res);
+//   //       const data = await res.json();
+//   //       console.log(data);
+//   //     }
+//   //     getUserData();
+//   //     setProviders(data);
+//   //   }, []);
 
-  return (
-    <ul>
-      {providers.map(item => {
-        return <li>{item.provider_name}</li>;
-      })}
-    </ul>
-  );
-}
+//   return (
+//     <ul>
+//       {providers.map(item => {
+//         return <li>{item.provider_name}</li>;
+//       })}
+//     </ul>
+//   );
+// }
 
-export default DisplayProviders;
+// export default DisplayProviders;
